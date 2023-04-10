15bb808ae217029da0148d963796a4d96c050257
Olena1010 <ebojcenko55@gmail.com>
2023-04-10 15:31:35 +0300

    second commit

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+};
+const startBtn = document.querySelector("[data-start]");
+const stopBtn = document.querySelector("[data-stop]");
+const body = document.querySelector("body");
 
+startBtn.addEventListener('click', onClickStart);
+stopBtn.addEventListener('click', onClickStop);
+
+let intervalId = null;
+
+function onClickStart() {
+    intervalId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+}, 1000);
+    startBtn.setAttribute('disabled', false);
+    stopBtn.removeAttribute("disabled");
+};
+function onClickStop() {
+    clearInterval(intervalId);
+    startBtn.removeAttribute("disabled");
+    stopBtn.setAttribute("disabled", false);
+}
